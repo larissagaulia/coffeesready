80a2a85f5954f98dafad4f5b893bacacadccb353
Larissa Gaulia <larissagaulia@gmail.com>
2017-09-15 19:38:18 -0300

    Init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
 #This file intializes a Python module. Without it, Python will not recognize the app directory as a module.
+
+from flask import Flask
+
+#Initialize the app
+
+app = Flask(__name__, instance_relative_config=True)
+
+#Load the views
+from app import views
+
+#Load the config file
+app.config.from_object('config')
