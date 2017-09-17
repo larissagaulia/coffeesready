91f2ae72d4f4786005647b13e4cb6ca1280e8e61
Larissa Gaulia <larissagaulia@gmail.com>
2017-09-17 19:05:45 -0300

    move db configuration to config file

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,11 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__, instance_relative_config=True)
 
-app.config['MONGO_DBNAME'] = 'connect_to_mongo'
-app.config['MONGO_URI'] = 'mongodb://pretty:printed@ds135594.mlab.com:35594/connect_to_mongo'
+#Load the config file
+app.config.from_object('config')
 
 mongo = PyMongo(app)
 
 #Load the views
 from app import views
 
-#Load the config file
-app.config.from_object('config')
--- config.py
+++ config.py
@@ -3,3 +3,6 @@
 
 # Enable Flask's debugging features. Should be False in production
 DEBUG = True
+MONGO_DBNAME = 'connect_to_mongo'
+MONGO_URI = 'mongodb://pretty:printed@ds135594.mlab.com:35594/connect_to_mongo'
+
