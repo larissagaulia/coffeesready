a60a89a0719f23f89c5282c42311b66e1fd8fe1a
Larissa Gaulia <larissagaulia@gmail.com>
2017-09-16 23:32:55 -0300

    connect db

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ from flask_pymongo import PyMongo
 #Initialize the app
 
 app = Flask(__name__, instance_relative_config=True)
+
+app.config['MONGO_DBNAME'] = 'connect_to_mongo'
+app.config['MONGO_URI'] = 'mongodb://pretty:printed@ds135594.mlab.com:35594/connect_to_mongo'
+
 mongo = PyMongo(app)
 
 #Load the views
--- app/views.py
+++ app/views.py
@@ -1,14 +1,22 @@
 #This file contains all the routes for our application.
 #This will tell Flask what to display on which path.
-
+from datetime import datetime
 from flask import render_template
 from flask import request
 
 from app import app
+from app import mongo
 
 @app.route('/')
 def index():
   return render_template("index.html")
+
+@app.route('/add')
+def add():
+  coffee_status = mongo.db.coffee_status
+  now = str(datetime.now())
+  coffee_status.insert({'status': 'done', 'timestamp': now})
+  return 'Coffee status added'
 
 @app.errorhandler(404)
 def not_found(error):
